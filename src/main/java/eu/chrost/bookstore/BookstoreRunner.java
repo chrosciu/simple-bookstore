afea4fe1aa121707ec8b8fc321980c8fe8cf03ee
Marcin Chrost <mchrost82@gmail.com>
2024-08-19 19:11:14 +0200

    Disable BookstoreRunner in tests

--- src/main/java/eu/chrost/bookstore/BookstoreRunner.java
+++ src/main/java/eu/chrost/bookstore/BookstoreRunner.java
@@ -3,9 +3,11 @@ package eu.chrost.bookstore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty("bookstore.runner.enabled")
 @RequiredArgsConstructor
 @Slf4j
 class BookstoreRunner implements CommandLineRunner {
